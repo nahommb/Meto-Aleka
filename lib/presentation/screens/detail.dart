9459508b6993c0a767996eb14adf45346ddad05c
Nahom Melese <nahomjr17@gmail.com>
2025-03-18 21:34:47 -0700

    updated

--- lib/presentation/screens/detail.dart
+++ lib/presentation/screens/detail.dart
@@ -15,9 +15,47 @@ class detailScreen extends StatelessWidget {
             IconButton(onPressed: (){
               Navigator.pop(context);
             }, icon: Icon(Icons.arrow_back_ios_outlined)),
-            Center(
-              child: Text('leee'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(onPressed: (){}, child: Row(
+                  children: [
+                    Icon(Icons.create_outlined),
+                    SizedBox(width: 5,),
+                    Text('Create'),
+                  ],
+                )),
+              ],
             ),
+            Container(
+              width: double.infinity,
+              height: 180,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                gradient: LinearGradient(
+                  colors: [Color(0xFF601FF1), Colors.black], // Gradient colors
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text('Nahom Lee',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 60.0),
+                    child: Text('450',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                  )
+                ],
+              ),
+            ),
+
+
           ],
         ),
       ),
