3dae32723c48a064d816c2d274a0aad9508f682a
Nahom Melese <nahomjr17@gmail.com>
2025-03-18 18:12:41 -0700

    updated

--- lib/presentation/widgets/data_list.dart
+++ lib/presentation/widgets/data_list.dart
@@ -12,23 +12,37 @@ class dataList extends StatelessWidget {
 
     return Expanded(
       child: Container(
-        padding: EdgeInsets.only(top: 30,left: 10),
+        padding: EdgeInsets.only(top: 0,left: 10,right: 10),
         height: screenHeight*0.6,
         decoration: BoxDecoration(
-          color: Colors.lightBlue,
-          borderRadius: BorderRadius.vertical(top: Radius.circular(20),)
+          gradient: LinearGradient(
+            colors: [Color(0xFF601FF1), Colors.black], // Gradient colors
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
         ),
+
         child: ListView.builder(
-          itemCount: 4,
+          itemCount: 14,
           itemBuilder: (context, index) =>
-            ListTile(
-              title: Text('Nahom Lee'),
-              subtitle: Text('Software'),
-              trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete)),
-              onTap: (){
-                print('clicked');
-                Navigator.pushNamed(context, detailScreen.route_name);
-              },
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(10))
+              ),
+              margin: EdgeInsets.only(top: 10),
+              child: ListTile(
+                title: Text('Nahom Lee'),
+                subtitle: Text('Software'),
+                trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete)),
+                onTap: (){
+                  print('clicked');
+                  Navigator.pushNamed(context, detailScreen.route_name);
+                },
+              ),
             ),
         ),
       ),
